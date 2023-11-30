a79250ac8b6a3fb92bfab3386d28366a74f2d65e
Nathan Rauh <nathan.rauh@us.ibm.com>
2023-11-30 16:45:13 -0600

    Updates to be used in JakartaOne demo
    Signed-off-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- src/main/java/io/openliberty/sample/application/CrewMembers.java
+++ src/main/java/io/openliberty/sample/application/CrewMembers.java
@@ -3,6 +3,8 @@ package io.openliberty.sample.application;
 import java.util.List;
 import java.util.stream.Stream;
 
+import jakarta.data.page.Page;
+import jakarta.data.page.Pageable;
 import jakarta.data.repository.DataRepository;
 import jakarta.data.repository.OrderBy;
 import jakarta.data.repository.Repository;
@@ -10,11 +12,13 @@ import jakarta.data.repository.Save;
 import jakarta.validation.Valid;
 
 @Repository
-public interface CrewMembers extends DataRepository<CrewMember, String> {
+public interface CrewMembers extends DataRepository<CrewMember, Integer> {
     @Save
     CrewMember save(@Valid CrewMember m);
     
     List<CrewMember> findByRank(Rank rank);
+
+    Page<CrewMember> findByRank(Rank rank, Pageable pageRequest);
 
     @OrderBy("name")
     Stream<CrewMember> findAll();
